c267aece05efaf57204d7cca083757b7b4e4949f
Josh Overbeek <joshoverbeek0@gmail.com>
2021-10-11 11:55:18 -0400

    Initial Commit

---
+++ FileSorter/MainWindow.Designer.cs
@@ -0,0 +1,75 @@
+﻿
+namespace FileSorter
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.folderSelector = new System.Windows.Forms.FolderBrowserDialog();
+            this.fileSelectButton = new System.Windows.Forms.Button();
+            this.folderInputText = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // fileSelectButton
+            // 
+            this.fileSelectButton.Location = new System.Drawing.Point(13, 13);
+            this.fileSelectButton.Name = "fileSelectButton";
+            this.fileSelectButton.Size = new System.Drawing.Size(154, 29);
+            this.fileSelectButton.TabIndex = 0;
+            this.fileSelectButton.Text = "select folder";
+            this.fileSelectButton.UseVisualStyleBackColor = true;
+            this.fileSelectButton.Click += new System.EventHandler(this.fileSelectButton_Click);
+            // 
+            // folderInputText
+            // 
+            this.folderInputText.Location = new System.Drawing.Point(185, 13);
+            this.folderInputText.Name = "folderInputText";
+            this.folderInputText.Size = new System.Drawing.Size(176, 27);
+            this.folderInputText.TabIndex = 1;
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.folderInputText);
+            this.Controls.Add(this.fileSelectButton);
+            this.Name = "MainWindow";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FolderBrowserDialog folderSelector;
+        private System.Windows.Forms.Button fileSelectButton;
+        private System.Windows.Forms.TextBox folderInputText;
+    }
+}
+
---
+++ FileSorter/MainWindow.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FileSorter
+{
+    public partial class MainWindow : Form
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void fileSelectButton_Click(object sender, EventArgs e)
+        {
+            DialogResult result = folderSelector.ShowDialog();
+            if ( result == DialogResult.OK)
+            {
+                folderInputText.Text = folderSelector.SelectedPath;
+            }
+        }
+    }
+}
